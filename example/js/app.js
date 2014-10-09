5cdbe198c6393d24c4e0869528826c0a6a7a718f
Jason Buffin <jason@tackk.com>
2014-10-09 16:43:12 -0400

    example app

---
+++ example/js/app.js
@@ -0,0 +1,15 @@
+$(function() {
+    MobilePages.page('home')
+        .addProperty(
+            'init',
+            function init() {
+
+            });
+
+    MobilePages.page('second')
+        .addProperty(
+            'init',
+            function init() {
+
+            });
+});
